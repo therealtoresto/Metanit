6ce66fa4962426f8c97d077cb11e0fe34e2b071b
therealtoresto <therealtoresto@gmail.com>
2022-12-04 13:29:37 +0200

    added using ejs engine example

---
+++ ExpressApp/g-ejs-engine/app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const express = require('express');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use('/contact', (req, res) => {
+    res.render('contact', {
+        title: 'My contacts',
+        emailsVisible: true,
+        emails: ['admin@gmail.com', 'user@gmail.com'],
+        phone: '+1234567890'
+    });
+});
+
+app.use('/', (req, res) => {
+    res.send('Main page');
+});
+
+app.listen(3000);
