ad3df5798ed2b6dd77ab56829704ec0f8964b155
therealtoresto <therealtoresto@gmail.com>
2022-12-11 05:37:24 +0200

    added model for users

---
+++ MVC/models/user.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const users = [];
+
+module.exports = class User{
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    save() {
+        users.push(this);
+    }
+    static getAll() {
+        return users;
+    }
+}
