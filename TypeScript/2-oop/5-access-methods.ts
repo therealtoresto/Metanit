728e582e0f21d76ff5bfb3ccbdd49599539ddf10
therealtoresto <therealtoresto@gmail.com>
2022-12-12 18:37:48 +0200

    added examples of access methods in TypeScript

---
+++ TypeScript/2-oop/5-access-methods.ts
@@ -0,0 +1,22 @@
+class Person {
+    name: string;
+    private _age: number;
+    private _name: string;
+
+    public get age(): number {
+        return this._age;
+    }
+
+    public set age(n: number) {
+        if (n < 0 || n > 110) {
+            console.log('Invalid age!');
+        } else this._age = n;
+    }
+}
+
+const tom = new Person();
+tom.name = 'Tom';
+tom.age = 36;
+console.log(`${tom.name}: ${tom.age}`); // Tom: 36
+tom.age = -1234;
+console.log(`${tom.name}: ${tom.age}`); // Tom: 36
