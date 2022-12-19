c8e07934f269202bf9dd654d28c917bb514c6fb6
therealtoresto <therealtoresto@gmail.com>
2022-12-19 20:29:23 +0200

    added example of using webpack

--- React/1-introduction/3-index.jsx
+++ React/1-introduction/3-index.jsx
@@ -1,3 +1,4 @@
+// npx babel 3-index.jsx --out-file index.js
 const rootNode = document.getElementById('app');
 const root = ReactDOM.createRoot(rootNode);
 root.render(
---
+++ React/1-introduction/4-app.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const fs = require('fs');
+
+http.createServer((req, res) => {
+    let filePath = '4-index.html';
+    if (req.url !== '/') {
+        filePath = req.url.substring(1);
+    }
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            res.statusCode = 404;
+            res.end('Resource not found!');
+        } else {
+            res.end(data);
+        }
+    });
+}).listen(3000, () => console.log('Server started at 3000 port'));
